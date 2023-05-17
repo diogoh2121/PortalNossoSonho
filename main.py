20a2570302935f41b635901fd18993a4ed778652
Aline de Carvalho Silva <93343538+AlineCarvalhoSilva@users.noreply.github.com>
2023-05-17 09:32:29 -0300

    Adicionando funcionalidade de navegação entre páginas, bem como início de implantação CSS.

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder= "templates_folder")
 
 # Criar a primeira página do site (toda página tem um route, uma função e um template)
-@app.route("/homepage")
-def homepage():
-    return render_template("homepage.html")
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-@app.route("/contatos")
-def contatos():
-    return render_template("contatos.html")
+@app.route("/materias")
+def materias():
+    return render_template("materias.html")
+
+@app.route("/hrcircular")
+def hrcircular():
+    return render_template("hrcircular.html")
+
+@app.route("/teluteis")
+def teluteis():
+    return render_template("teluteis.html")
 
 # Colocar o site no ar
 if __name__ == "__main__":
