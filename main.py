b00634aad4779a317dd94c23c128011543036d11
Aline de Carvalho Silva <93343538+AlineCarvalhoSilva@users.noreply.github.com>
2023-05-20 21:50:58 -0300

    Últimas Atualizações do projeto - Páginas HTML

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, session, request
+from flask_mysqldb import MySQL
 
-app = Flask(__name__, template_folder= "templates_folder")
+app = Flask(__name__)
+
+# Conectar o Banco de Dados
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '0252'
+app.config['MYSQL_HOST'] = '3306'
+app.config['MYSQL_DB'] = 'pi1'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+@app.route("/")
+def index():
+    cur = mysql.connection.cursor()
+    cur.execute('''CREATE TABLE Example (id INTEGER, nome VARCHAR(20))''')
+    return "Done!"
 
 # Criar a primeira página do site (toda página tem um route, uma função e um template)
 @app.route("/")
